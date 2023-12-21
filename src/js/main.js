c3f2df74b7946c7c859ac6bc5b4b9b9ce8bdf1e9
tenteshka <erkinbekovaasel34@gmail.com>
2023-12-21 18:42:18 +0600

    add: maps

--- src/js/main.js
+++ src/js/main.js
@@ -27,3 +27,16 @@ burger.addEventListener('click',() => {
     burger.classList.toggle("active")
     burgerMenu.classList.toggle("active")
 })
+
+let openOverlay = document.querySelector(".header__register")
+let overlay = document.querySelector(".overlay")
+let closeOverlay = document.querySelector(".close")
+openOverlay.addEventListener('click', ()  => {
+    overlay.style.display = 'flex'
+})
+closeOverlay.addEventListener('click', ()  => {
+    overlay.style.display = 'none'
+})
+overlay.addEventListener('click', (e)  => {
+    if (e.target.className === 'overlay') overlay.style.display = 'none'
+})
