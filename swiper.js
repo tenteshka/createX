0abaf8c011ca48a848dbc5318f61bc90f10942d5
tenteshka <erkinbekovaasel34@gmail.com>
2023-12-23 18:54:34 +0600

    layot

--- swiper.js
+++ swiper.js
@@ -29,5 +29,32 @@ var swiper2 = new Swiper(".team__swiper", {
     autoplay:{
         delay:2000
     },
-    loop: true
+    loop: true,
+    breakpoints:{
+        886:{
+            slidesPerView: 3,
+        },
+        768:{
+            slidesPerView: 3,
+        },
+        676:{
+            slidesPerView: 2,
+        },
+        576:{
+            slidesPerView:2,
+        },
+        500:{
+            slidesPerView:2,
+        },
+        425:{
+            slidesPerView:1,
+        },
+        375:{
+            slidesPerView:1,
+        },
+        320:{
+            slidesPerView:1,
+        },
+
+    }
 });
